b061abb8164242fab4279e3d087f3975b9bce6f1
Vishal <80979232+vishal-k25@users.noreply.github.com>
2024-01-03 16:05:13 +0530

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -27,10 +27,7 @@ function Feed() {
       </Box>
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
-        </Typography>
-
+      
         <Videos videos={videos} />
       </Box>
     </Stack>
